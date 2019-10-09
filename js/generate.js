4e904202c6563246541fa05ffd1ea3709d6d3ce7
NezFeeLitNoeud <nepheline.nehlig@gmail.com>
2019-10-09 20:48:53 +0200

    Start working on collision

--- js/generate.js
+++ js/generate.js
@@ -14,16 +14,29 @@ let Aquarium = function(){
 
     this.generationDePoisson = function(){
        for(let i = 0; i < this.poissonArray.length;i++){
+           this.collision();
            this.ctx.fillStyle = "red";
            this.ctx.fillRect(this.poissonArray[i].x,this.poissonArray[i].y, this.poissonArray[i].width, this.poissonArray[i].height)
            this.poissonArray[i].move();
+        //    console.log(this.poissonArray[i].x)
        }
-    }
 
+    
+    }
 
     this.update = function(){
         this.ctx.clearRect(0,0,500,500);
         this.generationDePoisson();
+    }
+// && this.poissonArray[j].y <= 0 || this.poissonArray[j].y >= 440
+//this.poissonArray[j].x <= 0 || this.poissonArray[j].x >= 480 
+    this.collision = function() {
+        for(let j = 0; j < this.poissonArray.length; j++){
+            if(this.poissonArray[j].y <= 100){
+                this.poissonArray[j].x -= this.poissonArray[j].random.x;
+                this.poissonArray[j].x -= this.poissonArray[j].random.x;
+            }
+        }
     }
 }
 
