f91d7dd00625fdd8c4245b82ee38e35341d694fa
NezFeeLitNoeud <nepheline.nehlig@gmail.com>
2019-10-10 21:25:38 +0200

    Added the collision with the border of the aquarium

--- js/generate.js
+++ js/generate.js
@@ -5,8 +5,8 @@ let Aquarium = function(){
     this.positionSourisX = 0;
     this.positionSourisY = 0;
     this.recupPosition = function(event){
-        this.positionSourisX = event.clientX;
-        this.positionSourisY = event.clientY;
+        this.positionSourisX = event.clientX - 310;
+        this.positionSourisY = event.clientY - 25;
     }  
     this.creationPoisson = function(){
         this.poissonArray.push(new Fish(this.positionSourisX, this.positionSourisY));
@@ -14,13 +14,20 @@ let Aquarium = function(){
 
     this.generationDePoisson = function(){
        for(let i = 0; i < this.poissonArray.length;i++){
-           this.collision();
+           this.collision(this.poissonArray[i]);
            this.ctx.fillStyle = "red";
            this.ctx.fillRect(this.poissonArray[i].x,this.poissonArray[i].y, this.poissonArray[i].width, this.poissonArray[i].height)
            this.poissonArray[i].move();
         //    console.log(this.poissonArray[i].x)
        }
 
+       this.collision = function(poisson) {
+        if(poisson.y <=0 || poisson.y >= 480){
+            poisson.random.y = -poisson.random.y;
+        } else if(poisson.x <= 0 || poisson.x >=480){
+         poisson.random.x = -poisson.random.x;
+        }
+     }
     
     }
 
@@ -28,8 +35,7 @@ let Aquarium = function(){
         this.ctx.clearRect(0,0,500,500);
         this.generationDePoisson();
     }
-// && this.poissonArray[j].y <= 0 || this.poissonArray[j].y >= 440
-//this.poissonArray[j].x <= 0 || this.poissonArray[j].x >= 480 
+    
     this.collision = function() {
         for(let j = 0; j < this.poissonArray.length; j++){
             if(this.poissonArray[j].y <= 100){
